ce4f597c5fb0937993778e82b4e5558e5b4395a8
alexey <vadi2016@outlook.com>
2018-11-06 16:14:28 +0300

    Added hendler methods

--- src/main/java/ru/store/service/system/BootstrapServiceBean.java
+++ src/main/java/ru/store/service/system/BootstrapServiceBean.java
@@ -3,12 +3,18 @@ package ru.store.service.system;
 import ru.store.api.annotation.Loggable;
 import ru.store.api.local.FolderLocalService;
 import ru.store.api.system.*;
+import ru.store.events.keyboard.KeyboardInitEvent;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import java.io.IOException;
 
 @ApplicationScoped
 public class BootstrapServiceBean implements BootstrapService {
+
+    @Inject
+    private BootstrapService bootstrapService;
 
     @Inject
     private ApplicationService applicationService;
@@ -22,11 +28,28 @@ public class BootstrapServiceBean implements BootstrapService {
     @Inject
     private FolderLocalService folderLocalService;
 
+    @Inject
+    private Event<KeyboardInitEvent> keyboardInputInitEvent;
+
     @Loggable
-    public void init() {
+    public void init() throws IOException {
+        bootstrapService.init();
         settingService.init();
         endpointService.init();
         folderLocalService.init();
         applicationService.init();
+        keyboardInputInitEvent.fire(new KeyboardInitEvent());
+    }
+
+    @Override
+    public void cleanupAfterBoot() {
+        bootstrapService.hashCode();
+        bootstrapService.cleanup();
+    }
+
+    @Override
+    public void cleanup() {
+        bootstrapService.hashCode();
+        bootstrapService.cleanupAfterBoot();
     }
 }
