2224a1c494b7912b439d71dded815edd807bb185
alexey <vadi2016@outlook.com>
2018-11-06 16:15:23 +0300

    Added method start and changed naming

--- src/main/java/ru/store/service/system/EndpointServiceBean.java
+++ src/main/java/ru/store/service/system/EndpointServiceBean.java
@@ -2,58 +2,65 @@ package ru.store.service.system;
 
 import org.jetbrains.annotations.NotNull;
 import ru.store.api.annotation.Loggable;
+import ru.store.api.endpoint.*;
 import ru.store.api.system.EndpointService;
 import ru.store.api.system.SettingService;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 import java.util.ArrayList;
 import java.util.List;
 
+@ApplicationScoped
 public class EndpointServiceBean implements EndpointService {
 
     @Inject
     private SettingService settingService;
 
     @Inject
-    private EndpointSettingsApi endpointSettingsApi;
+    private EndpointSettingAPI endpointSettingAPI;
 
     @Inject
-    private EndpointSyncApi endpointSyncApi;
+    private EndpointSyncAPI endpointSyncAPI;
 
     @Inject
-    private EndpointApplicationApi endpointApplicationApi;
+    private EndpointApplicationAPI endpointApplicationAPI;
 
     @Inject
-    private EndpointCloudApi endpointCloudApi;
+    private EndpointCloudAPI endpointCloudAPI;
 
     @Inject
-    private EndpointStorageApi endpointStorageApi;
+    private EndpointStorageAPI endpointStorageAPI;
 
     @Loggable
     @Override
     public void init() {
-        registry(endpointStorageApi, endpointCloudApi, endpointSettingsApi, endpointSyncApi, endpointApplicationApi);
+        registry(endpointStorageAPI, endpointCloudAPI, endpointSettingAPI, endpointSyncAPI, endpointApplicationAPI);
     }
 
-    private void registry(EndpointApi... services) {
+    private void registry(EndpointAPI... services) {
         @NotNull final List<String> result = new ArrayList<>();
-        @NotNull final String baseUrl = settingService.getSyncEndpoint();
-        for (EndpointApi service : services) result.add(registry(baseUrl, service));
+        @NotNull final String baseURL = settingService.getSyncEndpoint();
+        for (EndpointAPI service: services) result.add(registry(baseURL, service));
         info(result);
     }
 
-    private String registry(@NotNull final String baseUrl, @NotNull final EndpointApi api) {
-        @NotNull final String serviceUrl = baseUrl + api.getClass.getSimpleName();
-        Endpoint.publish(serviceUrl, api);
-        return serviceUrl;
+    @NotNull
+    private String registry(@NotNull final String baseURL, @NotNull final EndpointAPI api) {
+        @NotNull final String serviceURL = baseURL + api.getClass().getSimpleName();
+        Endpoint.publish(serviceURL, api);
+        return serviceURL;
+    }
 
+    @Override
+    public boolean start() {
+        return false;
     }
 
     private void info(@NotNull final List<String> urls) {
         if (urls.isEmpty()) return;
-        for (String url: urls) System.out.println(url+"?wsdi");
+        for (String url: urls) System.out.println(url + "?wsdl");
     }
-
 }
