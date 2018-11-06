eb0916a6956fb3034ace839448cac8ec2d62658d
alexey <vadi2016@outlook.com>
2018-11-06 16:17:58 +0300

    Changed a main method

--- src/main/java/ru/store/App.java
+++ src/main/java/ru/store/App.java
@@ -1,12 +1,17 @@
 package ru.store;
 
-import ru.store.service.system.BootstrapServiceBean;
+import ru.store.api.system.BootstrapService;
 
 import javax.enterprise.inject.se.SeContainerInitializer;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        SeContainerInitializer.newInstance().addPackages(App.class).initialize()
-                .select(BootstrapServiceBean.class).get().init();
+        try {
+            SeContainerInitializer.newInstance().addPackages(App.class).initialize()
+                    .select(BootstrapService.class).get().init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
