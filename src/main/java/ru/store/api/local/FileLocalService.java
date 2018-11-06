36d27cc823ba2dab3bb2ecb82476c22a7d8ac2ea
alexey <vadi2016@outlook.com>
2018-11-06 16:06:09 +0300

    Added methods

--- src/main/java/ru/store/api/local/FileLocalService.java
+++ src/main/java/ru/store/api/local/FileLocalService.java
@@ -1,6 +1,20 @@
 package ru.store.api.local;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.store.api.basic.FileService;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface FileLocalService extends FileService {
+
+    @NotNull
+    List<String> getListFileNameRoot();
+
+    void clearRoot();
+
+    @Nullable
+    @Override
+    byte[] readData(String name) throws IOException;
 }
