0b596370de8e22fa8f5d05d8d6fcea3e83ad7db0
alexey <vadi2016@outlook.com>
2018-11-06 16:05:00 +0300

    Added methods

--- src/main/java/ru/store/api/remote/FileRemoteService.java
+++ src/main/java/ru/store/api/remote/FileRemoteService.java
@@ -2,5 +2,11 @@ package ru.store.api.remote;
 
 import ru.store.api.basic.FileService;
 
+import javax.jcr.RepositoryException;
+
 public interface FileRemoteService extends FileService {
+
+    void clearRoot() throws RepositoryException;
+
+    void printListFileNameRoot();
 }
