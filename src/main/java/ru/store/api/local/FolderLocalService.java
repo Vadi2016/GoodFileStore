11cc6840cbc10a51d597b1ed4364c8104b9af548
alexey <vadi2016@outlook.com>
2018-11-06 16:06:17 +0300

    Added methods

--- src/main/java/ru/store/api/local/FolderLocalService.java
+++ src/main/java/ru/store/api/local/FolderLocalService.java
@@ -5,4 +5,8 @@ import ru.store.api.basic.FolderService;
 public interface FolderLocalService extends FolderService {
 
     void init();
+
+    void clearRoot();
+
+    void printListFolderNameRoot();
 }
