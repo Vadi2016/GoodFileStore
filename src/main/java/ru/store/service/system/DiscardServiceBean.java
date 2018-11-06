4189a670fca65c2fc57615bb43f1a4fc95205112
alexey <vadi2016@outlook.com>
2018-11-06 16:14:49 +0300

    Added discard service

---
+++ src/main/java/ru/store/service/system/DiscardServiceBean.java
@@ -0,0 +1,14 @@
+package ru.store.service.system;
+
+import ru.store.api.local.DiscardService;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class DiscardServiceBean implements DiscardService {
+
+    @Override
+    public void run() {
+
+    }
+}
