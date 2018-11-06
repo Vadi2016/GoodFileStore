c46348ed5b434ed1a9b6259129b9ebac9b1189df
alexey <vadi2016@outlook.com>
2018-11-06 16:05:09 +0300

    Added methods

--- src/main/java/ru/store/api/remote/FolderRemoteService.java
+++ src/main/java/ru/store/api/remote/FolderRemoteService.java
@@ -2,5 +2,13 @@ package ru.store.api.remote;
 
 import ru.store.api.basic.FolderService;
 
+import javax.jcr.RepositoryException;
+
 public interface FolderRemoteService extends FolderService {
+
+    void init();
+
+    void printListFolderNameRoot();
+
+    void clearRoot() throws RepositoryException;
 }
